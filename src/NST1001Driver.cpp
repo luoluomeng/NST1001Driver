823aeabeb50d74cf6fe17c3db482d2befc738038
HellDuke <luoluomengmeng@gmail.com>
2022-04-08 09:40:27 +0800

    optimize code

--- src/NST1001Driver.cpp
+++ src/NST1001Driver.cpp
@@ -10,16 +10,6 @@ static bool nst1001_isr_handler(mcpwm_unit_t mcpwm, mcpwm_capture_channel_id_t c
                                 void *arg)
 {
     BaseType_t high_task_wakeup = pdFALSE;
-    // if (tt != 0 && (millis() - tt) > 20)
-    // {
-    //     xQueueSendFromISR(cap_queue, &pulse_count, &high_task_wakeup);
-    //     pulse_count = 0;
-    //     Serial.printf("ticketElapsed:%d,last_paulse:%d \r\n", millis() - tt, edata->cap_value - last_paulse);
-    // }
-    // else
-    // {
-    //     pulse_count++;
-    // }
     tt = millis();
     if (last_paulse != 0)
     {
@@ -39,49 +29,15 @@ static bool nst1001_isr_handler(mcpwm_unit_t mcpwm, mcpwm_capture_channel_id_t c
     return high_task_wakeup == pdTRUE;
 }
 
-void pcntStartCounter()
-{
-    pcnt_counter_pause(PCNT_UNIT_0);
-    pcnt_counter_clear(PCNT_UNIT_0);
-    int err = pcnt_counter_resume(PCNT_UNIT_0);
-    if (err != ESP_OK)
-    {
-        return;
-    }
-}
-void pcntStopCounter()
-{
-
-    pcnt_counter_pause(PCNT_UNIT_0);
-    pcnt_counter_clear(PCNT_UNIT_0);
-}
-void pcntClearCounter()
-{
-    pcnt_counter_clear(PCNT_UNIT_0);
-}
-int16_t pcntGetCounter()
-{
-    int16_t count;
-    pcnt_get_counter_value(PCNT_UNIT_0, &count);
-    return count;
-}
-
 void initNST1001(int pin)
 {
-
-    // the queue where we read data
     cap_queue = xQueueCreate(1, sizeof(uint32_t));
     if (cap_queue == NULL)
     {
         return;
     }
-
-    /* configure Echo pin */
-    // set CAP_0 on GPIO
     ESP_ERROR_CHECK(mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM_CAP_0, pin));
-    // enable pull down CAP0, to reduce noise
-    ESP_ERROR_CHECK(gpio_pulldown_en((gpio_num_t)pin));
-    // enable both edge capture on CAP0
+
     mcpwm_capture_config_t conf = {
         .cap_edge = MCPWM_POS_EDGE,
         .cap_prescale = 1,
